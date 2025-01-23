7584960017d7d1426ce03f8df771803fbddcec47
jordantucker-ocadu <jordantucker@ocadu.ca>
2025-01-23 14:02:25 -0500

    First Version

---
+++ script.js
@@ -0,0 +1,47 @@
+0; // this is a comment, which means that javascript will not try to execute this part
+134151855;
+12341.1515;
+Infinity;
+-13434;
+-Infinity;
+-0;
+
+
+var kitty; // declared a variable to store space in memory;
+
+kitty = 5; // definition with a data type of number with the value 5
+
+var Kitty = 24; // semicolons are end of statement, this line has a definition and declarataion
+
+console.log(kitty);
+
+kitty = Kitty; // = assignment operator right to left
+
+console.log(kitty)
+
+var doggy; // declared a variable but not defined a value
+console.log(doggy); // undefined 
+
+// numbers, undefined, strings
+
+var prose = 'akdldfj lkja kjsdlkjdf lkj "lkljjfs" '; // 'is a string delimiter
+var moreprose = "i said 'something' sdfd"  ;
+
+//booleans are either true or fals
+
+var x = true;
+x = false;
+
+// objects
+var ball = {}; // empty object
+ball = null; // null is a datatype in javascript
+
+// operators
+5 + 5;
+kitty = 5 + 5;
+var kitty; // becomes undefined because we are now referring to a new place in memory
+
+kitty = 5 + "5"; //string concatmation operator
+console.log(kitty);
+
+console.log(kitty + "lkjhgf");
